d8422133e61175e5910e27b3f7b24ea31e7b91e2
StarMoon <2549007745@qq.com>
2025-11-23 09:37:57 +0800

    Add files via upload

---
+++ language.lua
@@ -0,0 +1,257 @@
+return {
+    ["zh-cn"] = {
+        ["Rainbow Friends"] = {
+            ["Tab"] = {
+                ["Main"] = "主要",
+                ["UI Settings"] = "界面设置"
+            },
+            ["Main 1"] = {
+                ["Main"] = "主要",
+                ["Auto PickUp item"] = "自动拾取物品",
+                ["Teleport Build Structures"] = "传送到结构"
+            },
+            ["Main 2"] = {
+                ["Esp"] = "透视",
+                ["Esp Item"] = "透视物品",
+                ["Esp Monsters"] = "透视怪物",
+                ["Esp Hight Light"] = "高亮显示",
+                ["Esp Gui"] = "远程界面显示",
+                ["Text Size [ Gui ]"] = "文字大小 [界面]",
+                ["Esp Name"] = "显示名称",
+                ["Esp Distance"] = "显示距离",
+                ["Esp Health"] = "显示生命值"
+            }
+        },
+        ["Ink Game"] = {
+            ["Tab"] = {
+                ["Main"] = "主要",
+                ["Misc"] = "杂项",
+                ["UI Settings"] = "界面设置"
+            },
+            ["Main"] = {
+                ["Green Light, Red Light"] = "红绿灯",
+                ["Help Player To End"] = "帮助玩家到终点",
+                ["Auto Help Player"] = "自动帮助玩家",
+                ["Troll Player"] = "捉弄玩家",
+                ["Auto Troll Player"] = "自动捉弄玩家",
+                ["Dalgona"] = "椪糖",
+                ["Complete Dalgona"] = "完成椪糖挑战",
+                ["Tug Of War / Hide & Seek"] = "拔河 / 捉迷藏",
+                ["Auto Tug of War"] = "自动拔河",
+                ["Esp Door Exit"] = "出口定位",
+                ["Esp Key"] = "钥匙定位",
+                ["Esp Player Hide"] = "定位躲藏者",
+                ["Esp Player Seek"] = "定位追捕者",
+                ["Auto Teleport Hide"] = "自动传送躲藏者",
+                ["Teleport Player Hide"] = "传送躲藏者",
+                ["Teleport All Key"] = "传送所有钥匙",
+                ["Jump Rope"] = "跳绳",
+                ["Complete Jump Rope"] = "完成跳绳",
+                ["Remove Jump Rope"] = "移除跳绳",
+                ["Glass Bridge"] = "玻璃桥",
+                ["Glass Bridge Vision"] = "识别真假玻璃",
+                ["Complete Glass Bridge"] = "完成玻璃桥",
+                ["Mingle"] = "交际",
+                ["Auto Mingle"] = "自动扭脖子",
+                ["Rebel"] = "反叛者",
+                ["WallCheck"] = "墙壁检测",
+                ["Aimbot Guard"] = "自动瞄准守卫",
+                ["Bring Guard"] = "召唤守卫",
+                ["Inf Ammo"] = "无限子弹"
+            },
+            ["Misc"] = {
+                ["Auto Skip"] = "自动跳过",
+                ["Speed"] = "速度",
+                ["Auto Speed"] = "自动速度设定",
+                ["No Cooldown Proximity"] = "无冷却范围",
+                ["Noclip"] = "穿墙",
+                ["Inf Jump"] = "无限跳跃",
+                ["Teleport Player"] = "传送到玩家",
+                ["Camlock Player / TP"] = "锁定摄像头 / 传送",
+                ["Choose Collect"] = "选择收集",
+                ["Auto Collect"] = "自动收集已选",
+                ["Anti Fling"] = "防止弹飞",
+                ["Anti Banana"] = "防香蕉皮",
+                ["Anti Lag"] = "防延迟",
+                ["Fly"] = "飞行",
+                ["Fly Speed"] = "飞行速度",
+                ["Settings Gui"] = "界面设置",
+                ["Esp Hight Light"] = "高亮显示",
+                ["Esp Gui"] = "远视功能",
+                ["Text Size [ Gui ]"] = "文字大小 [ 远视 ]",
+                ["Esp Name"] = "显示名字",
+                ["Esp Distance"] = "显示距离"
+            }
+        }
+    },
+    ["en"] = {
+        ["Rainbow Friends"] = {
+            ["Tab"] = {
+                ["Main"] = "Main",
+                ["UI Settings"] = "UI Settings"
+            },
+            ["Main 1"] = {
+                ["Main"] = "Main",
+                ["Auto PickUp item"] = "Auto PickUp item",
+                ["Teleport Build Structures"] = "Teleport Build Structures"
+            },
+            ["Main 2"] = {
+                ["Esp"] = "Esp",
+                ["Esp Item"] = "Esp Item",
+                ["Esp Monsters"] = "Esp Monsters",
+                ["Esp Hight Light"] = "Esp Hight Light",
+                ["Esp Gui"] = "Esp Gui",
+                ["Text Size [ Gui ]"] = "Text Size [ Gui ]",
+                ["Esp Name"] = "Esp Name",
+                ["Esp Distance"] = "Esp Distance",
+                ["Esp Health"] = "Esp Health"
+            }
+        },
+        ["Ink Game"] = {
+            ["Tab"] = {
+                ["Main"] = "Main",
+                ["Misc"] = "Misc",
+                ["UI Settings"] = "UI Settings"
+            },
+            ["Main"] = {
+                ["Green Light, Red Light"] = "Green Light, Red Light",
+                ["Help Player To End"] = "Help Player To End",
+                ["Auto Help Player"] = "Auto Help Player",
+                ["Troll Player"] = "Troll Player",
+                ["Auto Troll Player"] = "Auto Troll Player",
+                ["Dalgona"] = "Dalgona",
+                ["Complete Dalgona"] = "Complete Dalgona",
+                ["Tug Of War / Hide & Seek"] = "Tug Of War / Hide & Seek",
+                ["Auto Tug of War"] = "Auto Tug of War",
+                ["Esp Door Exit"] = "Esp Door Exit",
+                ["Esp Key"] = "Esp Key",
+                ["Esp Player Hide"] = "Esp Player Hide",
+                ["Esp Player Seek"] = "Esp Player Seek",
+                ["Auto Teleport Hide"] = "Auto Teleport Hide",
+                ["Teleport Player Hide"] = "Teleport Player Hide",
+                ["Teleport All Key"] = "Teleport All Key",
+                ["Jump Rope"] = "Jump Rope",
+                ["Complete Jump Rope"] = "Complete Jump Rope",
+                ["Remove Jump Rope"] = "Remove Jump Rope",
+                ["Glass Bridge"] = "Glass Bridge",
+                ["Glass Bridge Vision"] = "Glass Bridge Vision",
+                ["Complete Glass Bridge"] = "Complete Glass Bridge",
+                ["Mingle"] = "Mingle",
+                ["Auto Mingle"] = "Auto Mingle",
+                ["Rebel"] = "Rebel",
+                ["WallCheck"] = "WallCheck",
+                ["Aimbot Guard"] = "Aimbot Guard",
+                ["Bring Guard"] = "Bring Guard",
+                ["Inf Ammo"] = "Inf Ammo"
+            },
+            ["Misc"] = {
+                ["Auto Skip"] = "Auto Skip",
+                ["Speed"] = "Speed",
+                ["Auto Speed"] = "Auto Speed",
+                ["No Cooldown Proximity"] = "No Cooldown Proximity",
+                ["Noclip"] = "Noclip",
+                ["Inf Jump"] = "Inf Jump",
+                ["Teleport Player"] = "Teleport Player",
+                ["Camlock Player / TP"] = "Camlock Player / TP",
+                ["Choose Collect"] = "Choose Collect",
+                ["Auto Collect"] = "Auto Collect",
+                ["Anti Fling"] = "Anti Fling",
+                ["Anti Banana"] = "Anti Banana",
+                ["Anti Lag"] = "Anti Lag",
+                ["Fly"] = "Fly",
+                ["Fly Speed"] = "Fly Speed",
+                ["Settings Gui"] = "Settings Gui",
+                ["Esp Hight Light"] = "Esp Hight Light",
+                ["Esp Gui"] = "Esp Gui",
+                ["Text Size [ Gui ]"] = "Text Size [ Gui ]",
+                ["Esp Name"] = "Esp Name",
+                ["Esp Distance"] = "Esp Distance"
+            }
+        }
+    },
+    ["fr"] = {
+        ["Rainbow Friends"] = {
+            ["Tab"] = {
+                ["Main"] = "Principal",
+                ["UI Settings"] = "Paramètres UI"
+            },
+            ["Main 1"] = {
+                ["Main"] = "Principal",
+                ["Auto PickUp item"] = "Ramassage Auto d'objets",
+                ["Teleport Build Structures"] = "Téléportation Structures"
+            },
+            ["Main 2"] = {
+                ["Esp"] = "ESP",
+                ["Esp Item"] = "ESP Objets",
+                ["Esp Monsters"] = "ESP Monstres",
+                ["Esp Hight Light"] = "Surbrillance ESP",
+                ["Esp Gui"] = "Interface ESP",
+                ["Text Size [ Gui ]"] = "Taille Texte [ Interface ]",
+                ["Esp Name"] = "Afficher Nom",
+                ["Esp Distance"] = "Afficher Distance",
+                ["Esp Health"] = "Afficher Santé"
+            }
+        },
+        ["Ink Game"] = {
+            ["Tab"] = {
+                ["Main"] = "Principal",
+                ["Misc"] = "Divers",
+                ["UI Settings"] = "Paramètres UI"
+            },
+            ["Main"] = {
+                ["Green Light, Red Light"] = "Feu Vert, Feu Rouge",
+                ["Help Player To End"] = "Aider Joueur vers Fin",
+                ["Auto Help Player"] = "Aide Auto Joueur",
+                ["Troll Player"] = "Troller Joueur",
+                ["Auto Troll Player"] = "Troll Auto Joueur",
+                ["Dalgona"] = "Dalgona",
+                ["Complete Dalgona"] = "Compléter Dalgona",
+                ["Tug Of War / Hide & Seek"] = "Tir à la Corde / Cache-cache",
+                ["Auto Tug of War"] = "Tir à la Corde Auto",
+                ["Esp Door Exit"] = "ESP Porte Sortie",
+                ["Esp Key"] = "ESP Clé",
+                ["Esp Player Hide"] = "ESP Joueur Cache",
+                ["Esp Player Seek"] = "ESP Joueur Cherche",
+                ["Auto Teleport Hide"] = "Téléport Auto Cacheur",
+                ["Teleport Player Hide"] = "Téléport Cacheur",
+                ["Teleport All Key"] = "Téléport Toutes Clés",
+                ["Jump Rope"] = "Corde à Sauter",
+                ["Complete Jump Rope"] = "Compléter Corde",
+                ["Remove Jump Rope"] = "Supprimer Corde",
+                ["Glass Bridge"] = "Pont de Verre",
+                ["Glass Bridge Vision"] = "Vision Pont Verre",
+                ["Complete Glass Bridge"] = "Compléter Pont Verre",
+                ["Mingle"] = "Socialiser",
+                ["Auto Mingle"] = "Casse-cou Auto",
+                ["Rebel"] = "Rebelle",
+                ["WallCheck"] = "Vérif Mur",
+                ["Aimbot Guard"] = "Aimbot Garde",
+                ["Bring Guard"] = "Amener Garde",
+                ["Inf Ammo"] = "Munitions Infinies"
+            },
+            ["Misc"] = {
+                ["Auto Skip"] = "Passer Auto",
+                ["Speed"] = "Vitesse",
+                ["Auto Speed"] = "Vitesse Auto",
+                ["No Cooldown Proximity"] = "Pas Recharge Proximité",
+                ["Noclip"] = "NoClip",
+                ["Inf Jump"] = "Saut Infini",
+                ["Teleport Player"] = "Téléport Joueur",
+                ["Camlock Player / TP"] = "Camlock Joueur / TP",
+                ["Choose Collect"] = "Choisir Collecte",
+                ["Auto Collect"] = "Collecte Auto",
+                ["Anti Fling"] = "Anti Fling",
+                ["Anti Banana"] = "Anti Banane",
+                ["Anti Lag"] = "Anti Lag",
+                ["Fly"] = "Voler",
+                ["Fly Speed"] = "Vitesse Vol",
+                ["Settings Gui"] = "Paramètres GUI",
+                ["Esp Hight Light"] = "Surbrillance ESP",
+                ["Esp Gui"] = "ESP Visuel",
+                ["Text Size [ Gui ]"] = "Taille Texte [ Visuel ]",
+                ["Esp Name"] = "Afficher Nom",
+                ["Esp Distance"] = "Afficher Distance"
+            }
+        }
+    }
+}
